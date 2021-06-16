3a72abdfe8494de86361e43b38a20bff30bf9877
James Montgomery <james@jmontgomery.uk>
2021-06-16 04:44:17 +0100

    initial commit

---
+++ code.js
@@ -0,0 +1,11 @@
+grained('#container', {
+  "animate": true,
+  "patternWidth": 500,
+  "patternHeight": 100,
+  "grainOpacity": 0.12,
+  "grainDensity": 1,
+  "grainWidth": 1,
+  "grainHeight": 1
+})
+
+console.log(grained)
